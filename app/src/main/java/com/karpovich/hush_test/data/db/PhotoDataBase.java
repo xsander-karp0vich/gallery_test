08e0e33ceb88572e5becdb41c58f34e2ba4bc3dc
alex <natalia42010822@mail.ru>
2023-11-24 16:56:55 +0300

    Added and configure PhotoDataBase

--- app/src/main/java/com/karpovich/hush_test/data/db/PhotoDataBase.java
+++ app/src/main/java/com/karpovich/hush_test/data/db/PhotoDataBase.java
@@ -1,5 +1,28 @@
 package com.karpovich.hush_test.data.db;
 
-public class PhotoDataBase {
+import android.app.Application;
 
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.karpovich.hush_test.data.entities.Photo;
+
+@Database(entities = {Photo.class},version = 1,exportSchema = false)
+public abstract class PhotoDataBase extends RoomDatabase {
+
+    private static final String DB_NAME = "photo.db";
+    private static PhotoDataBase instance = null;
+
+    public static PhotoDataBase getInstance(Application application){
+        if (instance == null){
+            instance = Room.databaseBuilder(
+                    application,
+                    PhotoDataBase.class,
+                    DB_NAME
+            ).build();
+        }
+        return instance;
+    }
+    public abstract PhotoDao photoDao();
 }
