87fa8a0388730b1520278cd33f0f968adbdf8295
alex <natalia42010822@mail.ru>
2023-11-24 16:19:21 +0300

    Added and configure PhotoDao

--- app/src/main/java/com/karpovich/hush_test/data/db/PhotoDao.java
+++ app/src/main/java/com/karpovich/hush_test/data/db/PhotoDao.java
@@ -1,4 +1,25 @@
 package com.karpovich.hush_test.data.db;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.karpovich.hush_test.data.entities.Photo;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Single;
+
+@Dao
 public interface PhotoDao {
+    @Query("SELECT * FROM photos" )
+    LiveData<List<Photo>> getAllPhotos();
+
+    @Insert
+    Completable insertPhoto (Photo photo);
+
+    @Query("DELETE FROM photos WHERE id = :photoId")
+    Completable removePhoto(int photoId);
 }
