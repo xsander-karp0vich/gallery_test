3fc5b9ee79db3ae8caa394073aea6a41d4671750
alex <natalia42010822@mail.ru>
2023-11-24 12:40:19 +0300

    Added and configured main activity view model

--- app/src/main/java/com/karpovich/hush_test/ui/activities/MainActivity.java
+++ app/src/main/java/com/karpovich/hush_test/ui/activities/MainActivity.java
@@ -1,21 +1,26 @@
 package com.karpovich.hush_test.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.View;
 
 import com.karpovich.hush_test.R;
 import com.karpovich.hush_test.databinding.ActivityMainBinding;
+import com.karpovich.hush_test.ui.viewmodels.MainActivityViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private MainActivityViewModel viewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
     }
 }
---
+++ app/src/main/java/com/karpovich/hush_test/ui/viewmodels/MainActivityViewModel.java
@@ -0,0 +1,14 @@
+package com.karpovich.hush_test.ui.viewmodels;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class MainActivityViewModel extends AndroidViewModel {
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+
+    }
+
+}
