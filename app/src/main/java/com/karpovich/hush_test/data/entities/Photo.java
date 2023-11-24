82964a0e277d6229e47a3cc954c5f31749d723ee
alex <natalia42010822@mail.ru>
2023-11-24 16:04:21 +0300

    Added the photo entity

--- app/src/main/java/com/karpovich/hush_test/data/entities/Photo.java
+++ app/src/main/java/com/karpovich/hush_test/data/entities/Photo.java
@@ -1,4 +1,40 @@
 package com.karpovich.hush_test.data.entities;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "photos")
 public class Photo {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String title;
+    private String uri;
+    public Photo(int id, String title, String uri) {
+        this.id = id;
+        this.title = title;
+        this.uri = uri;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
 }
