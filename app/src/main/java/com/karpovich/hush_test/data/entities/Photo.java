6c5c7c5032a615571de14c9e14cb39045234046e
alex <natalia42010822@mail.ru>
2023-11-29 06:28:55 +0300

    updated photo class

--- app/src/main/java/com/karpovich/hush_test/data/entities/Photo.java
+++ app/src/main/java/com/karpovich/hush_test/data/entities/Photo.java
@@ -10,12 +10,22 @@ public class Photo {
     @PrimaryKey(autoGenerate = true)
     private int id;
     private String title;
+    private String description;
     private String uri;
+
+    public Photo(int id, String title, String description, String uri) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.uri = uri;
+    }
+
     public Photo(int id, String title, String uri) {
         this.id = id;
         this.title = title;
         this.uri = uri;
     }
+
     public int getId() {
         return id;
     }
@@ -30,6 +40,14 @@ public class Photo {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getUri() {
